74c17725655a719b27340f87a0d8ce837d659e1a
Dan Lüdtke <dan.luedtke@egym.de>
2018-01-04 13:32:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mobile-update-check
+
+
+## compile protobuf to grpc
+
+    protoc --go_out=plugins=grpc:. *.proto
---
+++ client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	uc "github.com/danrl/update-check/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	serverAddress := flag.String("server-address", "localhost:8080", "Address of update check server to query.")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		log.Fatalf("missing version")
+	}
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("dial: %v", err)
+	}
+	defer conn.Close()
+
+	ucClient := uc.NewUpdateCheckClient(conn)
+
+	action, err := ucClient.VersionCheck(context.Background(), &uc.UpdateVersion{
+		Version: flag.Args()[0],
+	})
+	if err != nil {
+		log.Fatalf("version check: %v", err)
+	}
+	fmt.Printf("%v\n", action)
+
+}
