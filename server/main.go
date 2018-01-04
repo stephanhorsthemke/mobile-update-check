00401b6d822367e9c19bc5aaa2fb48548761d01f
Dan Lüdtke <dan.luedtke@egym.de>
2018-01-04 17:06:19 +0100

    added Dockerfile

--- server/main.go
+++ server/main.go
@@ -8,44 +8,44 @@ import (
 
 	"google.golang.org/grpc"
 
-	uc "github.com/egymgmbh/mobile-update-check/pb"
+	pb "github.com/egymgmbh/mobile-update-check/pb"
 	"github.com/Masterminds/semver"
 )
 
 
 type rule struct {
-	os uc.OSType
+	os pb.OSType
 	osVersion string
 	osVersionConstraints *semver.Constraints
-	product uc.ProductType
+	product pb.ProductType
 	productVersion string
 	productVersionConstraints *semver.Constraints
-	action uc.ResponseAction
+	action pb.ResponseAction
 }
 
 type ucs struct {
 }
 
 var (
-	_ uc.UpdateCheckServiceServer = &ucs{}
+	_ pb.UpdateCheckServiceServer = &ucs{}
  	rules []rule
  )
 
 func init() {
 	rules := []rule{
 		{
-				os: uc.OSType_ANDROID,
+				os: pb.OSType_ANDROID,
 				osVersion: ">=8.0.0, <9.0.0",
-				product: uc.ProductType_FITAPP,
+				product: pb.ProductType_FITAPP,
 				productVersion: "<=2.3.0",
-				action: uc.ResponseAction_ADVICE,
+				action: pb.ResponseAction_ADVICE,
 		},
 		{
-				os: uc.OSType_IOS,
+				os: pb.OSType_IOS,
 				osVersion: "9.0.0",
-				product: uc.ProductType_TRAINERAPP,
+				product: pb.ProductType_TRAINERAPP,
 				productVersion: "2.3.0",
-				action: uc.ResponseAction_FORCE,
+				action: pb.ResponseAction_FORCE,
 		},
 	}
 
@@ -74,17 +74,17 @@ func (r *rule) CompileConstraints() error {
 		return nil
 }
 
-func (r *rule) Apply(req *uc.UpdateCheckRequest, osVersion, productVersion *semver.Version) (uc.ResponseAction, bool) {
+func (r *rule) Apply(req *pb.UpdateCheckRequest, osVersion, productVersion *semver.Version) (pb.ResponseAction, bool) {
 	if req.OS == r.os &&
 	 req.Product == r.product  &&
 	 r.osVersionConstraints.Check(osVersion) &&
 	 r.productVersionConstraints.Check(productVersion) {
 		return r.action, true
 	}
-	return uc.ResponseAction_NONE, false
+	return pb.ResponseAction_NONE, false
 }
 
-func (u *ucs) Query(ctx context.Context, in *uc.UpdateCheckRequest) (*uc.UpdateCheckResponse, error) {
+func (u *ucs) Query(ctx context.Context, in *pb.UpdateCheckRequest) (*pb.UpdateCheckResponse, error) {
 	osVersion, err := semver.NewVersion(in.OSVersion)
 	if err != nil {
 		return nil, fmt.Errorf("malformed request")
@@ -95,13 +95,13 @@ func (u *ucs) Query(ctx context.Context, in *uc.UpdateCheckRequest) (*uc.UpdateC
 	}
 	for _, rule := range rules {
 		if action, ok := rule.Apply(in, osVersion, productVersion); ok {
-			return &uc.UpdateCheckResponse{
+			return &pb.UpdateCheckResponse{
 				Action: action,
 			}, nil
 		}
 	}
-	return &uc.UpdateCheckResponse{
-		Action: uc.ResponseAction_NONE,
+	return &pb.UpdateCheckResponse{
+		Action: pb.ResponseAction_NONE,
 	}, nil
 }
 
@@ -111,6 +111,6 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	uc.RegisterUpdateCheckServiceServer(grpcServer, &ucs{})
+	pb.RegisterUpdateCheckServiceServer(grpcServer, &ucs{})
 	grpcServer.Serve(lis)
 }
